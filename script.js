8f1aaeeec91d9f7e3542bdc03d7483200174f9d4
Anna Ostrowska-Kus <anna.ostrowska@wp.pl>
2021-11-28 18:08:51 +0100

    add status.id

--- script.js
+++ script.js
@@ -26,7 +26,27 @@ const getWeather = () =>{
         humidity.textContent = hum +'%'
         weather.textContent = status.main
 
-    })
+        if(status.id>200 && status.id<231){
+            photo.setAttribute('src', './img/thunderstorm.png')
+        }else if(status.id>300 && status.id<321){
+                photo.setAttribute('src', './img/drizzle.png')
+        }else if(status.id>500 && status.id<531){
+            photo.setAttribute('src', './img/rain.png')
+        }else if(status.id>600 && status.id<622){
+            photo.setAttribute('src', './img/ice.png')
+        }else if(status.id==701 && status.id< 781){
+            photo.setAttribute('src', './img/fog.png')
+        }else if(status.id==800){
+            photo.setAttribute('src', './img/sun.png')
+        }else if(status.id>801 && status.id<804){
+            photo.setAttribute('src', './img/cloud.png')
+        }else{
+            photo.setAttribute('src', './img/unknown.png')
+        }
+
+    }    
+
+    )
 }
 
 button.addEventListener('click',getWeather)
